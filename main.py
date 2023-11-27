fed131a2df06ffe84a151d63c5bdd46517ec691a
Ashton Teng <ashtonteng@gmail.com>
2023-11-26 17:22:31 -0800

    add way to bulk process videos

--- main.py
+++ main.py
@@ -4,26 +4,12 @@ from util import *
 from gpt import *
 import openai
 
-if __name__ == "__main__":
-    load_dotenv()
-    openai_client = openai.Client()
 
-    directories_to_create = [
-        '0_transcripts',
-        '1_extracted',
-        '2_refined',
-        '3_formatted',
-        '4_final_json'
-    ]
-    for directory in directories_to_create:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-    url = input("Enter youtube url: ")
+def youtube_url_to_json(openai_client, url: str) -> str:
     video_id = extract_youtube_video_id(url)
     print("getting transcript for video id: ", video_id)
     transcript = get_transcript_from_youtube_video(video_id)
-    with open(f'0_transcripts/{video_id}', 'w') as f:
+    with open(f'0_transcripts/{video_id}.txt', 'w') as f:
         f.write(transcript)
 
     print("extracting protocol for video id: ", video_id)
@@ -58,3 +44,28 @@ if __name__ == "__main__":
           f"{metadata['title']}")
     with open(f'4_final_json/{video_id}.json', 'w') as f:
         f.write(formatted_json)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    openai_client = openai.Client()
+
+    directories_to_create = [
+        '0_transcripts',
+        '1_extracted',
+        '2_refined',
+        '3_formatted',
+        '4_final_json'
+    ]
+    for directory in directories_to_create:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    url = input("Enter youtube url: ")
+    youtube_url_to_json(openai_client, url)
+
+    # seed_urls = open('seed_urls.txt', 'r').readlines()
+    # print(seed_urls)
+    # for seed_url in seed_urls:
+    #     url = seed_url.strip()
+    #     youtube_url_to_json(openai_client, url)
