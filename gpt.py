d65f77a07eac2ffc9189cb97230434e5e53a6669
Ashton Teng <ashtonteng@gmail.com>
2023-11-28 16:18:12 -0800

    also add extract protocol using assistant api

--- gpt.py
+++ gpt.py
@@ -1,3 +1,7 @@
+from typing import List
+import time
+
+
 def extract_protocol(client, text: str) -> str:
     extractor_prompt = open('prompts/extractor.txt', 'r').read()
     chat_completion = client.chat.completions.create(
@@ -15,6 +19,48 @@ def extract_protocol(client, text: str) -> str:
         temperature=0.3,
     )
     return chat_completion.choices[0].message.content
+
+
+def assistant_extract_protocol(client, chunks: List[str]) -> str:
+    responses = []
+    extractor_prompt = open('prompts/extractor.txt', 'r').read()
+    # file = client.files.create(
+    #     file=open('...', 'rb'),
+    #     purpose="assistants",
+    # )
+    assistant = client.beta.assistants.create(
+        name="Video Transcript Health Actions Extractor",
+        instructions=extractor_prompt,
+        # tools=[{"type": "retrieval"}],
+        # file_ids=[file.id],
+        model="gpt-4-1106-preview",
+    )
+    thread = client.beta.threads.create()
+
+    # TODO: parallelize this
+    for chunk in chunks:
+        _ = client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=chunk,
+        )
+        run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id=assistant.id,
+            # instructions=extractor_prompt,
+        )
+        while run.status != 'completed':
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread.id,
+                run_id=run.id
+            )
+            print(run.status)
+            time.sleep(5)
+        messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+        responses.append(messages.data[0].content[0].text.value)
+    return '\n'.join(responses)
 
 
 def refine_protocol(client, text: str) -> str:
